0a20252b0f9812887211dbe4a304ed66589f70df
budhil <budhilnigam@gmail.com>
2026-03-08 22:50:27 +0530

    Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,4 +16,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
 })
